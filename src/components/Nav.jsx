ed673f8dafce68417e2a5ce2b6070465e1b7091a
ame <amedouillard@hotmail.fr>
2023-01-18 18:26:42 +0100

    Ajoute gestion de isOwned dans getAllKata

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -23,7 +23,7 @@ function Nav({userAddress, setUserAddress}) {
         <button className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 onClick={connectWallet}
         >
-          <span>Connect you with MetaMask</span>
+          <span>Connect to MetaMask</span>
         </button>
       );
     }
--- src/models/kata.js
+++ src/models/kata.js
@@ -1,9 +1,10 @@
 export class Kata {
-  constructor(id, name, statement, functionDeclaration) {
+  constructor(id, name, statement, functionDeclaration, isOwned) {
     this.id = id;
     this.name = name;
     this.statement = statement;
     this.functionDeclaration = functionDeclaration;
+    this.isOwned = isOwned;
   }
 
 }
